3f7c72e86430952f3befc41cf698746dbbc24d53
Sajan Kumar <121430388+Sajan-12@users.noreply.github.com>
2025-05-15 01:16:48 +0530

    second commit
    bcryptjs used

--- Back-End/User/user.controller.js
+++ Back-End/User/user.controller.js
@@ -1,5 +1,5 @@
 const userModel=require("./user.schema.js");
-const bcrypt=require('bcrypt');
+const bcrypt=require('bcryptjs');
 const jwt=require("jsonwebtoken");
 
 const signUp=async(req,res)=>{
