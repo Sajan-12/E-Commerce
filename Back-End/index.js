623d52ec33edcfbb3ee49256af7362fb12688a36
Sajan Kumar <121430388+Sajan-12@users.noreply.github.com>
2025-05-15 20:16:38 +0530

    third commit
    updated upload url

--- Back-End/index.js
+++ Back-End/index.js
@@ -22,7 +22,7 @@ app.use('/images',express.static('upload/images'));
 app.post('/upload',upload.single('product'),(req,res)=>{
     res.json({
         success:"true",
-        image_url:`http://localhost:${port}/images/${req.file.filename}`
+        image_url:`https://e-commerce-8waw.onrender.com/images/${req.file.filename}`
     })
 })
 
